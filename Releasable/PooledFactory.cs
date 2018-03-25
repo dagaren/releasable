a777c105f3d98de6513ba7678976b199bb2d952b
David Garcinuño Enríquez <dagaren@gmail.com>
2018-03-25 21:51:16 +0200

    Added PooledFactory implementation of IReleasableFactory

---
+++ Releasable/PooledFactory.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="PooledFactory.cs" company="David Garcinuño Enríquez">
+// Copyright (c) David Garcinuño Enríquez. All rights reserved.
+// </copyright>
+
+namespace Dagaren.Releasable
+{
+    using System;
+
+    public class PooledFactory<T> : IReleasableFactory<T>
+    {
+        private readonly IPool<T> pool;
+
+        private readonly object synco = new object();
+
+        public PooledFactory(IPool<T> pool)
+        {
+            this.pool = pool ?? throw new ArgumentNullException();
+        }
+
+        public IReleasable<T> Get()
+        {
+            lock (this.synco)
+            {
+                return new ReusableReleasable<T>(this.pool.Get(), this.Release);
+            }
+        }
+
+        private void Release(T obj)
+        {
+            lock (this.synco)
+            {
+                this.pool.Return(obj);
+            }
+        }
+    }
+}
