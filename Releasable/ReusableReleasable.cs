e8bf304d1e295ff62e9cfa5de4ec7ab51024964b
David Garcinuño Enríquez <dagaren@gmail.com>
2018-03-25 17:54:13 +0200

    Added ReusableReleasable implementation of IReleasable

---
+++ Releasable/ReusableReleasable.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="ReusableReleasable.cs" company="David Garcinuño Enríquez">
+// Copyright (c) David Garcinuño Enríquez. All rights reserved.
+// </copyright>
+
+namespace Dagaren.Releasable
+{
+    using System;
+
+    public class ReusableReleasable<T> : IReleasable<T>
+    {
+        private Action<T> reuseAction;
+
+        public ReusableReleasable(T value, Action<T> reuseAction)
+        {
+            this.Value = value;
+
+            this.reuseAction = reuseAction ?? throw new ArgumentNullException(nameof(reuseAction));
+        }
+
+        public T Value { get; private set; }
+
+        public void Dispose()
+        {
+            this.reuseAction(this.Value);
+            this.Value = default(T);
+        }
+    }
+}
