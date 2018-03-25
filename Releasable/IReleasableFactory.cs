47079435175e1e5294724801c167bae512bf7c4b
David Garcinuño Enríquez <dagaren@gmail.com>
2018-03-25 18:03:15 +0200

    Added IReleasableFactory interface

---
+++ Releasable/IReleasableFactory.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="IReleasableFactory.cs" company="David Garcinuño Enríquez">
+// Copyright (c) David Garcinuño Enríquez. All rights reserved.
+// </copyright>
+
+namespace Dagaren.Releasable
+{
+    public interface IReleasableFactory<T>
+    {
+        IReleasable<T> Get();
+    }
+}
