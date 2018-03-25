8c1f58cc7c29d9cf7dacc7b27a8f8dbc057d4a5b
David Garcinuño Enríquez <dagaren@gmail.com>
2018-03-25 17:46:51 +0200

    Added DisposableReleasble implementation of IReleasable

---
+++ Releasable/DisposableReleasable.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="DisposableReleasable.cs" company="David Garcinuño Enríquez">
+// Copyright (c) David Garcinuño Enríquez. All rights reserved.
+// </copyright>
+
+namespace Dagaren.Releasable
+{
+    using System;
+
+    public class DisposableReleasable<T> : IReleasable<T>
+    {
+        public DisposableReleasable(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public void Dispose()
+        {
+            IDisposable disposable = this.Value as IDisposable;
+            if (disposable != null)
+            {
+                disposable.Dispose();
+            }
+
+            this.Value = default(T);
+        }
+    }
+}
