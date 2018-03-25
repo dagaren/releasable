f9da9bd7092b0024726d8499d5484e96b0365fbc
David Garcinuño Enríquez <dagaren@gmail.com>
2018-03-25 21:35:20 +0200

    Added IPool interface and implementation

---
+++ Releasable/IPool.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="IPool.cs" company="David Garcinuño Enríquez">
+// Copyright (c) David Garcinuño Enríquez. All rights reserved.
+// </copyright>
+
+namespace Dagaren.Releasable
+{
+    public interface IPool<T>
+    {
+        int NumElements { get; }
+
+        T Get();
+
+        void Return(T obj);
+    }
+}
---
+++ Releasable/Pool.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="Pool.cs" company="David Garcinuño Enríquez">
+// Copyright (c) David Garcinuño Enríquez. All rights reserved.
+// </copyright>
+
+namespace Dagaren.Releasable
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Pool<T> : IPool<T>
+    {
+        private readonly Queue<T> elements;
+
+        private readonly Func<T> generator;
+
+        public Pool(Func<T> generator)
+        {
+            this.elements = new Queue<T>();
+
+            this.generator = generator ?? throw new ArgumentNullException(nameof(generator));
+        }
+
+        public int NumElements
+        {
+            get
+            {
+                return this.elements.Count;
+            }
+        }
+
+        public T Get()
+        {
+            if (this.elements.Count > 0)
+            {
+                return this.elements.Dequeue();
+            }
+            else
+            {
+                return this.generator();
+            }
+        }
+
+        public void Return(T obj)
+        {
+            this.elements.Enqueue(obj);
+        }
+    }
+}
